5e3f0b92ad3b77e4d03f493a5b592e8e7c504d8e
Mrleoneed <mrleoneed@gmail.com>
2023-06-16 13:30:05 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    );
+}
+
+const Header = () => {
+    return (
+        <>
+            <a href={'#s'}>Home</a>
+            <a href={'#s'}>News Fees</a>
+            <a href={'#s'}>Messages</a>
+        </>
+    )
+}
+
+export default App;
