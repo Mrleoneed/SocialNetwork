6a69f3ad9d32909787682d0f51b86c5e03d2c332
Mrleoneed <mrleoneed@gmail.com>
2023-07-10 14:22:19 +0300

    15 CDD

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from './Post.module.css'
+
+
+export const Post = () => {
+    return (
+        <div>
+
+            <div className={s.item}>
+                <img
+                    src={'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/213245707/original/66a67e36fe8227d15c8c310cc112b60e74af5d6f/design-avatar-cartoon-for-business-gaming-social-media.jpg'}
+                    alt={'Avatar'}/>
+                post1
+
+                <div>
+                    <span>Like</span>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
+
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            Контент
+            <div>
+                <img
+                    src="https://www.breakflip-awe.com/uploads2/Pierre-Louis/the-social-netwrok-streaming.png"
+                    alt="cover"/>
+            </div>
+            <div>
+                AVATAR
+            </div>
+            <MyPosts/>
+        </div>
+    );
+};
+
+export default Profile;
