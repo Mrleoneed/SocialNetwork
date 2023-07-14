31ca538fa003fc28f7a5227fc12cdac12f9702a5
Mrleoneed <mrleoneed@gmail.com>
2023-07-14 10:47:10 +0300

    18 add Dialogs + css

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img
                     src="https://www.breakflip-awe.com/uploads2/Pierre-Louis/the-social-netwrok-streaming.png"
